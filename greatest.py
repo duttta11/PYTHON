b3c3119271aedc020a0b97223b43f531f49f261c
Aditya Dutta <adityadutta482@gmail.com>
2026-03-09 23:17:49 +0530

    Update greatest.py

--- greatest.py
+++ greatest.py
@@ -1,6 +1,6 @@
 a=float(input("enter first number"))
 b=float(input("enter second number"))
-  c=float(input("enter third number"))
+c=float(input("enter third number"))
  if a>b and b>c:
     print("a is greatest")
 elif b>c:
