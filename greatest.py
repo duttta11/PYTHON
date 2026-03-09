c98f530d810747de176d132ab2e960593d514c2c
Aditya Dutta <adityadutta482@gmail.com>
2026-03-09 23:18:12 +0530

    Add program to find the greatest of three numbers

--- greatest.py
+++ greatest.py
@@ -1,7 +1,7 @@
 a=float(input("enter first number"))
 b=float(input("enter second number"))
 c=float(input("enter third number"))
- if a>b and b>c:
+if a>b and b>c:
     print("a is greatest")
 elif b>c:
     print("b is greatest")
