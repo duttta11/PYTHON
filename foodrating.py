77d24b9d32d08434db69b884dba65d7043ab6bd0
Aditya Dutta <adityadutta482@gmail.com>
2026-03-16 22:50:50 +0530

    Create foodrating.py

---
+++ foodrating.py
@@ -0,0 +1,11 @@
+rating= float(input("enter a enter a rating(1-5)"))
+if rating>4.5:
+  print("extraordinary")
+elif rating>4:
+  print("excellent")
+elif rating >3:
+  print("good")
+elif rating>2:
+  print("fair")
+else:
+  print("poor")
