c61b183099f8374e921868963adf86bc117759b3
Aditya Dutta <adityadutta482@gmail.com>
2026-04-08 23:25:20 +0530

    Update Sum.py

--- Sum.py
+++ Sum.py
@@ -1,4 +1,4 @@
-a=int(input("enter first number"))
-b=int(input("enyer second number"))
-sum=a+b
+  a=int(input("enter first number"))
+  b=int(input("enyer second number"))
+  sum=a+b
 print("the sum of a and b is",sum)
